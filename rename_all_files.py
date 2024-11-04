5dbaeae4aa5247f746119a7d29be0c98810d5416
daniintech <reich.daniela@proton.me>
2024-11-04 13:40:38 +0100

    Update rename_all_files.py

--- rename_all_files.py
+++ rename_all_files.py
@@ -3,7 +3,7 @@ import os
 #for all files in a specified folder (path1), adds specified name at the very beginning of file
 #this was used for creating a better overview of the transcripts in terms of which youtuber they belonged to
 #useful for working in AntConc
-path1 = 'C:/Users/jawie/Documents/Uni/Bachelor/Transcript_py/csv_Incels/test'
+path1 = ''
 files = os.listdir(path1)
 
 #changes working directory to path specified in path1
@@ -11,7 +11,7 @@ os.chdir(path= path1)
 
 
 #name that should be added at the beginning of the file (name of youtuber the transcripts belong to)
-youtuber_name = "LoneWolf"
+youtuber_name = ""
 
 for file in files:
     os.rename(file, youtuber_name + file)
